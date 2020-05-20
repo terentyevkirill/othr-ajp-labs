b492fcc0f3225887587fa8d363cc78944d8c9a86
terentyevkirill <kirillterentyev1999@gmail.com>
2020-05-20 13:12:17 +0200

    static imports added for Collectors and Comparator. Output optimized. Added some collect-groupingBy solutions

--- LambdasAndStreams/src/com/othr/ajp/lambdasstreams/Main.java
+++ LambdasAndStreams/src/com/othr/ajp/lambdasstreams/Main.java
@@ -2,7 +2,9 @@ package com.othr.ajp.lambdasstreams;
 
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
 
 public class Main {
 
@@ -12,65 +14,95 @@ public class Main {
         // use the cities list and implement the following use cases using only streams and lambdas
 
         // print distinct list of cities on console
-//        cities.stream().distinct().forEach(System.out::println);
+        System.out.println("print distinct list of cities on console:");
+        cities.stream().distinct().forEach(System.out::println);
+        System.out.println("----------------------------------------------------");
 
         // print first 3 cities in list
-//        cities.stream().limit(3).forEach(System.out::println);
+        System.out.println("print first 3 cities in list:");
+        cities.stream().limit(3).forEach(System.out::println);
+        System.out.println("----------------------------------------------------");
 
         // store in boolean variable whether city names have all at least 6 characters
-//        System.out.println("All names have length of at least 6 chars: " + cities.stream().noneMatch(s -> s.length() < 6));
+        System.out.println("store in boolean variable whether city names have all at least 6 characters:");
+        System.out.println("All names have length of at least 6 chars: "
+                + cities.stream().noneMatch(s -> s.length() < 6));
+        System.out.println("----------------------------------------------------");
 
-        // store list of distinct city names in descending order of name's length (and pring to check):
-//        cities.stream().distinct().sorted(Comparator.comparingInt(String::length).reversed()).forEach(System.out::println);
-        // or
-//        cities.stream().distinct().sorted((s1, s2) -> s2.length()-s1.length()).forEach(System.out::println);
+        // store list of distinct city names in descending order of name's length (and print to check):
+        System.out.println("store list of distinct city names in descending order of name's length (and print to check):");
+        cities.stream().distinct().sorted(comparingInt(String::length).reversed()).forEach(System.out::println);
+        System.out.println("----------------------------------------------------");
 
         // store set of distinct city names in CAPITAL LETTERS in new TreeSet (and print to check):
-//        cities.stream().map(String::toUpperCase).collect(Collectors.toCollection(TreeSet::new)).forEach(System.out::println);
+        System.out.println("store set of distinct city names in CAPITAL LETTERS in new TreeSet (and print to check):");
+        cities.stream().map(String::toUpperCase).collect(toCollection(TreeSet::new)).forEach(System.out::println);
+        System.out.println("----------------------------------------------------");
 
         // find first city name in natural order of list of given length len and - if present - store name in String variable or store string "no city of length ..." (use terminal operation that returns Optional<T> object and continue using this object):
-//        final int len = 11;
-//        cities.stream().filter(s -> s.length() == len).findFirst().ifPresentOrElse(System.out::println, () -> System.out.println("no city name of length " + len));
+        System.out.println("find first city name in natural order of list of given length len and - if present - store name in String variable or store string \"no city of length ...\" (use terminal operation that returns Optional<T> object and continue using this object):");
+        final int len = 11;
+        cities.stream().filter(s -> s.length() == len)
+                .findFirst().ifPresentOrElse(System.out::println, () -> System.out.println("no city name of length " + len));
+        System.out.println("----------------------------------------------------");
 
         // print name of city with longest name (one if there are more)
-//        cities.stream().max(Comparator.comparingInt(String::length)).ifPresent(System.out::println);
+        System.out.println("print name of city with longest name (one if there are more):");
+        cities.stream().max(comparingInt(String::length)).ifPresent(System.out::println);
+        System.out.println("----------------------------------------------------");
 
         // store length of longest (or shortest) city name (and print variable to check)
-//        cities.stream().max(Comparator.comparingInt(String::length)).ifPresent(s -> System.out.println("length of longest name: " + s.length()));
-//        cities.stream().min(Comparator.comparingInt(String::length)).ifPresent(s -> System.out.println("length of shortest name: " + s.length()));;
+        System.out.println("store length of longest (or shortest) city name (and print variable to check)");
+        cities.stream().max(comparingInt(String::length))
+                .ifPresent(s -> System.out.println("length of longest name: " + s.length()));
+        // or
+        cities.stream().min(comparingInt(String::length))
+                .ifPresent(s -> System.out.println("length of shortest name: " + s.length()));
+        System.out.println("----------------------------------------------------");
 
         // reduce list of names to String of their initials
-//        System.out.print("Initials: ");
-//        cities.stream().map(s -> s.charAt(0)).forEach(System.out::print);
-//        System.out.println();
+        System.out.println("reduce list of names to String of their initials:");
+        System.out.print("Initials: ");
+        cities.stream().map(s -> s.charAt(0)).forEach(System.out::print);
+        System.out.println();
+        System.out.println("----------------------------------------------------");
 
         // compute total sum of string length over all names (and print to check)
-//        cities.stream().map(String::length).reduce(Integer::sum).ifPresent(s -> System.out.println("total string length over all names: " + s));
+        System.out.println("compute total sum of string length over all names (and print to check):");
+        cities.stream().map(String::length).reduce(Integer::sum).ifPresent(s -> System.out.println("total string length over all names: " + s));
+        System.out.println("----------------------------------------------------");
 
         // store a Map<Character,Long> with number of cities grouped by their initials (and print to check)
-//        Map<Character, Long> result = cities.stream().map(s -> s.toUpperCase().charAt(0)).distinct()
-//                .collect(Collectors.toMap(Character::charValue, c -> cities.stream().filter(s -> s.toUpperCase().charAt(0) == c).count()));
-//        result.forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println("store a Map<Character,Long> with number of cities grouped by their initials (and print to check):");
+        Map<Character, Long> result = cities.stream()
+                .collect(groupingBy(s -> s.charAt(0), counting()));
+        result.forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println("----------------------------------------------------");
 
-        //as above but do not store but print directly to console
-//        cities.stream().map(s -> s.toUpperCase().charAt(0)).distinct()
-//                .collect(Collectors.toMap(Character::charValue, c -> cities.stream().filter(s -> s.toUpperCase().charAt(0) == c).count()))
-//                .forEach((k, v) -> System.out.println(k + ": " + v));
+        // as above but do not store but print directly to console
+        System.out.println("as above but do not store but print directly to console:");
+        cities.stream()
+                .collect(groupingBy(s -> s.charAt(0), counting()))
+                .forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println("----------------------------------------------------");
 
         // as above but print map sorted by value
-//        Map<Character, Long> result = cities.stream().map(s -> s.toUpperCase().charAt(0)).distinct()
-//                .collect(Collectors.toMap(Character::charValue,
-//                        c -> cities.stream().filter(s -> s.toUpperCase().charAt(0) == c).count()));
-//        result = result.entrySet()
-//                .stream()
-//                .sorted(Map.Entry.comparingByValue())
-//                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-//        result.forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println("as above but print map sorted by value:");
+        result = cities.stream()
+                .collect(groupingBy(s -> s.charAt(0), counting()));
+        result = result.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        result.forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println("----------------------------------------------------");
 
         // count number of letters in city names and print table to console sorted by key:
-//        Map<Character, Long> result =
-//                cities.stream().map(s -> s.chars().mapToObj(c -> (char) c).toArray(Character[]::new)).flatMap(Arrays::stream)
-//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-//        result.forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println("count number of letters in city names and print table to console sorted by key:");
+        result = cities.stream()
+                .map(s -> s.chars().mapToObj(c -> (char) c).toArray(Character[]::new)).flatMap(Arrays::stream)
+                .collect(groupingBy(Function.identity(), counting()));
+        result.forEach((k, v) -> System.out.println(k + ": " + v));
+        System.out.println("----------------------------------------------------");
     }
 }
